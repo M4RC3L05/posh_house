ce076ef3993f669342f4502710dd5540af2366a1
João Baptista <joaobat@live.com.pt>
2019-03-07 20:00:50 +0000

    add ssh helpers
    Signed-off-by: João Baptista <joaobat@live.com.pt>

--- poshrc.ps1
+++ poshrc.ps1
@@ -2,7 +2,15 @@
 Import-Module "$PSScriptRoot\posh_functions\git\gitHelpers.psm1"
 Import-Module "$PSScriptRoot\posh_functions\path\pathHelpers.psm1"
 Import-Module "$PSScriptRoot\posh_functions\os\osHelpers.psm1"
+Import-Module "$PSScriptRoot\posh_functions\ssh\sshHelpers.psm1"
 Import-Module "$PSScriptRoot\posh_themes\themes\future\future.theme.psm1"
+
+
+# SSH-AGENT AUTOSTART
+$agent = Get-SshAgent
+if ($agent -eq 0) {
+	Start-SshAgent
+}
 
 function prompt {
     Theme
