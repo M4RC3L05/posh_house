10cf87e666e42acd7e694ad96d2e71de3b35662c
João Baptista <joaobat@live.com.pt>
2019-03-08 17:03:03 +0000

    update aliases
    Signed-off-by: João Baptista <joaobat@live.com.pt>

--- posh_alias.ps1
+++ posh_alias.ps1
@@ -1,15 +1,127 @@
-function goHome {
-    Set-Location  "${HOME}/Desktop"
-}
-
-function callMan {
-    & "C:\Users\joaob\scoop\apps\nodejs-lts\current\bin\tldr.cmd" -t ocean $args
-}
 
 Remove-Item alias:* -Force
 
 Set-Alias cd Set-Location -Option AllScope
-Set-Alias cls Clear-Host -Option AllScope
-Set-Alias dk goHome -Option AllScope
-Set-Alias man callMan -Option AllScope
-Set-Alias ls "C:\Users\joaob\scoop\shims\ls.exe" -Option AllScope
+
+function dk {
+    Set-Location  "${HOME}/Desktop"
+}
+
+function man {
+    tldr -t ocean $args
+}
+
+function ls {
+    C:\Users\joaob\scoop\shims\ls.exe --color=always $args
+}
+
+function mkdir {
+    C:\Users\joaob\scoop\shims\mkdir.exe $args
+}
+
+function fsize {
+    C:\Users\joaob\scoop\shims\du.exe -hcs $args
+}
+
+# ----------------------
+# Git Aliases
+# ----------------------
+function g {
+    git $args
+}
+
+function ga {
+    g add $args
+}
+
+function gaa {
+    git add .
+}
+
+function gaaa {
+    git add --all $args
+}
+
+function gau {
+    git add --update $args
+}
+
+function gb {
+    git branch $args
+}
+
+function gbd {
+    git branch --delete
+}
+
+function gc {
+    git commit $args
+}
+
+function gco {
+    git checkout $args
+}
+
+function gd {
+    git diff $args
+}
+
+function ginit {
+    git init $args
+}
+
+function glg {
+    git log --graph --oneline --decorate --all $args
+}
+
+function gm {
+    git merge $args
+}
+
+function gp {
+    git pull $args
+}
+
+function gpr {
+    git pull --rebase $args
+}
+
+function gr {
+    git rebase $args
+}
+
+function gs {
+    git status $args
+}
+
+function gss {
+    git status --short $args
+}
+
+function gst {
+    git stash $args
+}
+
+function gsta {
+    git stash apply $args
+}
+
+function gstd {
+    git stash drop $args
+}
+
+function gstl {
+    git stash list $args
+}
+
+function gstp {
+    git stash pop $args
+}
+
+function gsts {
+    git stash save $args
+}
+
+function gpush {
+    git push $args
+}
