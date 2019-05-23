70470e9ccfecc89d4812de65bea38a49ec876971
João Baptista <joaobat@live.com.pt>
2019-05-23 22:10:58 +0100

    update to powershell 6
    Signed-off-by: João Baptista <joaobat@live.com.pt>

--- poshrc.ps1
+++ poshrc.ps1
@@ -4,7 +4,7 @@
 . "$PSScriptRoot\posh_functions\os\osHelpers.ps1"
 
 Import-Module posh-git
-Import-Module PSReadLine
+# Import-Module PSReadLine
 Import-Module Recycle
 Import-Module "$PSScriptRoot\posh_themes\ThemeProvider.psm1"
 
